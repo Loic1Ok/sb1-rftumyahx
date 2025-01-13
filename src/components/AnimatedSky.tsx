b940d703b5fe8a005cba1c826f94326b706c409f
Loic1Ok <103406396+Loic1Ok@users.noreply.github.com>
2025-01-13 09:07:29 +0400

    Update AnimatedSky.tsx

--- src/components/AnimatedSky.tsx
+++ src/components/AnimatedSky.tsx
@@ -8,12 +8,14 @@ export default function AnimatedSky() {
         <div className="cloud-1 animate-float-slow"></div>
         <div className="cloud-2 animate-float-medium"></div>
         <div className="cloud-3 animate-float-fast"></div>
+        <div className="cloud-4 animate-float-fast"></div>
       </div>
       
       {/* Animated birds */}
       <div className="absolute w-full h-full">
         <div className="bird-1 animate-fly-1"></div>
         <div className="bird-2 animate-fly-2"></div>
+        <div className="bird-3 animate-fly-3"></div>
       </div>
     </div>
   );
